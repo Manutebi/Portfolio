797f87a79f2145dbd63b9e50e18adfb30b2a3742
Manutebi <ma.aninir@duocuc.cl>
2023-11-05 01:19:27 -0300

    email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 
 import { fadeIn } from '../variants';
 
-import React, { useRef } from 'react';
+import { useRef } from 'react';
 
 import emailjs from '@emailjs/browser';
 
